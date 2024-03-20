ecd67fedd0f0dc740d4284748091ab951cd9f406
Rude Karine <rude.karine@gmail.com>
2024-03-20 18:32:55 +0100

    mise en place des informations du footer dans des fenêtres modales

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,82 @@
 <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
     <ul class="nav list-unstyled d-flex">
-        <li class="ms-3"><a class="text-body-secondary" href="#">Mentions Légales</a></li>
-        <li class="ms-3"><a class="text-body-secondary" href="#">Politiques de confidentialités</a></li>
-        <li class="ms-3"><a class="text-body-secondary" href="#">Crédits</a></li>
+        <li class="ms-3 nav-item"><a class="text-body-secondary" href="" data-bs-toggle="modal" data-bs-target="#ModalMentionsLegales">Mentions Légales</a></li>
+        <li class="ms-3 nav-item"><a class="text-body-secondary" href="" data-bs-toggle="modal" data-bs-target="#ModalPolitiqueConfidential">Politiques de confidentialités</a></li>
+        <li class="ms-3 nav-item"><a class="text-body-secondary" href="" data-bs-toggle="modal" data-bs-target="#ModalCredits">Crédits</a></li>
     </ul>
+
+    <!-- Modal Mentions Legales -->
+    <div class="modal fade" id="ModalMentionsLegales" tabindex="-1" aria-labelledby="ModalMentionsLegalesLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalMentionsLegalesLabel">Mentions Legales du site :</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h2> Conditions générales d'utilisation du site : </h2> 
+                    Toutes les informations présentes sur ce site sont entièrement fictives, car il s'agit d'un site web élaboré dans le cadre d'un examen en cours de formation.
+                    <hr>
+                    <h2>Propriétaire du site :</h2>
+                        <h5>KGB Agency</h5>
+                        Agence de renseignements au service de l'état, dans la lutte contre toute forme d'agissement illégal ou visant à nuire à la sécurité nationale.
+                    <hr>
+                    <h5>hébergeur du site internet :</h5>
+                        <h6>hostinger.fr</h6>
+                        Hébergement Web Premium, Cloud, VPS, Créateur de Sites Internet IA et Services d'Enregistrement de Noms de Domaine.
+                    <hr>
+                    <h5>Les mentions relatives à la propriété intellectuelle : </h5>
+                    Toutes les images de ce site sont issues de banques d'images libres de droits (notamment du site Pixabay).
+                    Le logo de ce site a été réalisé à l'aide d'outils graphiques présents dans le traitement de texte Word du pack Office 365.
+                    <hr>
+                    <h5>Les mentions relatives à l'utilisation des données personnelles et à la gestion des cookies : </h5>
+                    Voir notre politique de confidentialité
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal Politique de confidentialité -->
+    <div class="modal fade" id="ModalPolitiqueConfidential" tabindex="-1" aria-labelledby="ModalPolitiqueConfidentialLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalPolitiqueConfidentialLabel">Politique de confidentialité :</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h2>Utilisation des données personnelles :</h2> 
+                    <p>Sur notre site, le respect de votre vie privée est pour nous très important.</p> 
+                    <p>Toutefois, en tant qu'agence responsable de la sûreté de l'état, nous accumulons des informations sensibles sur un certain nombre de personnes 
+                        que nous surveillons activement afin d'éviter tout acte de terrorisme ou de mise en danger de nos concitoyens.</p>
+                    <p>En effet, ces informations sont nécessaires afin de pouvoir vous garantir une sécurité maximale sur notre territoire.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal Credits -->
+    <div class="modal fade" id="ModalCredits" tabindex="-1" aria-labelledby="ModalCreditsLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalCreditsLabel">Crédits :</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Nous remercions vivements toutes les personnes qui par leur bravoure ou leurs renseignements, nous permettent de mener à bien notre mission.
+                    Bien entendu, nous ne les citerons pas explicitement afin de ne pas les mettre en danger.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
