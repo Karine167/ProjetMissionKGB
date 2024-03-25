90317d3cfa5dc7cf1466350940a4325a58d3338a
Rude Karine <rude.karine@gmail.com>
2024-03-25 19:19:54 +0100

    Amélioration du CSS de la page d'authentification

--- templates/authentification.php
+++ templates/authentification.php
@@ -2,21 +2,26 @@
 require_once _TEMPLATEPATH_.'/header.php';
 ?>
 <main> 
-    <h1> Se connecter </h1>
-
-    <form method="POST">
-        <div class="mb-3 ms-1">
-            <div class="mb-3 ms-1">
-                <label for="email" class="form-label">Email :</label>
-                <input type="email" class="form-control" id="email" name="email">
-            </div>
-            <div class="mb-3 ms-1">
-                <label for="password" class="form-label">Mot de passe :</label>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-            <input type="submit" name="connect" class="btn btn-primary" value="Se connecter">
+    <div class="row d-flex justify-content-center ">
+        <div class="col-9 m-3 p-3 d-flex align-items-center justify-content-center pageTitle">
+            <h1> Se connecter </h1>
         </div>
-    </form>
+        <div class="col-11 m-3 p-3 formConnect">
+            <form method="POST">
+                <div class="mb-5 ms-1">
+                    <div class="mt-3 mb-5 mx-2">
+                        <label for="email" class="form-label">Email :</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="mt-3 mb-5 mx-2">
+                        <label for="password" class="form-label">Mot de passe :</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+                    <input type="submit" name="connect" class="m-3 btn btn-primary" value="Se connecter">
+                </div>
+            </form>
+        </div>
+    </div>
 
 </main>
 <?php
