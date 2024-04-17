7a60fffcf0aab860d55554663eed15a5ce13a5be
Rude Karine <rude.karine@gmail.com>
2024-04-17 08:43:41 +0200

    Ajout d'une fin de ligne et du mode ajouter pour le logErrors

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ use App\Controller\Controller;
 ini_set('display_errors', '0');
 // Définition d'un gestionnaire d'exception global
 set_exception_handler(function (Throwable $e){
-    $filelog = fopen('logError.txt', 'w');
+    $filelog = fopen('logError.txt', 'a');
     $date = date('d-m-Y H:i:s');
-    $error = $date.", " . $e->getMessage() . ", " . $e->getCode() . ", " . $e->getFile() . ", " . $e->getLine();
+    $error = $date.", " . $e->getMessage() . ", " . $e->getCode() . ", " . $e->getFile() . ", " . $e->getLine() . PHP_EOL;
     error_log($error, 3, 'logError.txt');
     fclose($filelog);
     $control = new Controller();
